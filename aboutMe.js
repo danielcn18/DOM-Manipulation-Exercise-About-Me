0641225f248ebe61703f2e18ab6f9a70161ce7ca
Daniel Chiquete <115588244+danielcn18@users.noreply.github.com>
2023-02-14 17:04:13 -0700

    Add files via upload

---
+++ aboutMe.js
@@ -0,0 +1,17 @@
+document.body.style.fontFamily = 'Arial, sans-serif';
+var nickname = document.getElementById('nickname');
+nickname.innerHTML = 'daniel';
+var favorites = document.getElementById('favorites');
+favorites.innerHTML = 'fried rice';
+var hometown = document.getElementById('hometown');
+hometown.innerHTML = 'tolleson';
+
+var li = document.querySelectorAll('li');
+for(var i = 0; i < li.length; i++){
+    li[i].classList.add('list-item');
+}
+var myImg = document.createElement('img');
+myImg.src = '/image/myRepresentation.png';
+myImg.style.width = '40%';
+myImg.style.height = '35vh';
+(document.body).appendChild(myImg);
